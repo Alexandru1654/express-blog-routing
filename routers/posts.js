3ca67dfb150a615c7e473242a48e1a086dd02235
alexandru233847 <alexutualexino33@gmail.com>
2024-11-04 18:26:16 +0100

    Search Posts

--- routers/posts.js
+++ routers/posts.js
@@ -6,3 +6,14 @@ router.get('/', (req, res) => {
     res.send('<ul>' + posts.map(post => `<li>${post.title}</li>`).join('') + '</ul>');
 });
 
+router.get('/:slug', (req, res) => {
+    const post = posts.find(p => p.slug === req.params.slug);
+    if (post) {
+        res.json(post);
+    } else {
+        res.status(404).send('Post not found');}
+    });
+
+    module.exports = router;
+
+    
